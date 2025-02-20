2acae5ba45ddd48146c72161decb96011d7ba05a
JACOB B STEPHEN <94170866+jacobbstephen@users.noreply.github.com>
2025-02-20 22:56:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const cors = require('cors');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectToDB = require('./config/db')
+connectToDB();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+const userRouter = require('./routes/userRoutes');
+app.use('/user', userRouter);
+
+const incidentRouter = require('./routes/incidentRoutes');
+app.use('/incident', incidentRouter)
+
+const emergencyRouter = require('./routes/emergencyRoutes')
+app.use('/emergency', emergencyRouter);
+
+const volunteerRouter = require('./routes/volunteerRoutes');
+app.use('/volunteer', volunteerRouter)
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log('Server Running on port 3000');
+})
